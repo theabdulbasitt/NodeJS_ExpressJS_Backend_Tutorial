6bf36ed6ab830c6d8046efea1043e4dc315d302a
Abdul Basit <163563761+theabdulbasitt@users.noreply.github.com>
2025-12-07 00:46:00 +0500

    integerating jwt

---
+++ 6/controllers/authController.js
@@ -0,0 +1,39 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+};
+
+const bcrypt = require('bcrypt');
+const { ChildProcess } = require('child_process');
+const jwt = require('jsonwebtoken');
+require('dotenv').config;
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'username and Password are required.' });
+    const foundUser = usersDB.users.find(person => person.username === user);
+    if (!foundUser) return res.sendStatus(401).json({ "Message": "No user found" }); //unauthorized
+    // evaluate password
+    const match = await bcrypt.compare(pwd, foundUser.password);
+    if (match) {
+        // creat jwts
+        const accessToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' }
+        );
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+        res.json({ 'success': `User ${user} is logged in!` });
+    } else {
+        res.sendStatus(401).json({ 'message': 'Incorrect password' });
+    }
+
+}
+module.exports = { handleLogin };
--- 6/server.js
+++ 6/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const { error } = require('console');
@@ -11,22 +12,9 @@ const PORT = process.env.PORT || 3500;
 app.use(logger);
 
 // Cross Origin Resource Sharing
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-}
 app.use(cors(corsOptions));
 
-// built-in middleware to handle urlencoded data
-// in other words, form data:  
-// ‘content-type: application/x-www-form-urlencoded’
+// built-in middleware to handle urlencoded data form data
 app.use(express.urlencoded({ extended: false }));
 
 // built-in middleware for json 
@@ -34,53 +22,13 @@ app.use(express.json());
 
 //serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/subdir', express.static(path.join(__dirname, '/public')));
+app.use('/views', express.static(path.join(__dirname, '/public')));
 
-//app.use('/', require('./routes/api'));
-app.use('/subdir', require('./routes/subdir'));
+// routes
+app.use('/', require('./routes/root'));
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
 app.use('/employees', require('./routes/api/employees'));
-
-app.get(/^\/$|\/index(.html)?$/, (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-
-app.get(/^\/new-page(.html)?$/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-});
-
-app.get(/^\/old-page(.html)?$/, (req, res) => {
-    res.redirect(301, '/new-page.html'); //302 by default
-});
-
-// Route handlers
-app.get(/^\/hello(.html)?$/, (req, res, next) => {
-    console.log('attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('Hello World!');
-});
-
-
-// chaining route handlers
-const one = (req, res, next) => {
-    console.log('one');
-    next();
-}
-
-const two = (req, res, next) => {
-    console.log('two');
-    next();
-}
-
-const three = (req, res) => {
-    console.log('three');
-    res.send('Finished!');
-}
-
-app.get(/^\/chain(.html)?$/, [one, two, three]);
-
-
 
 app.all(/.*/, (req, res) => {
     res.status(404);
@@ -92,8 +40,6 @@ app.all(/.*/, (req, res) => {
         res.type('txt').send("404 Not found");
     }
 });
-
-
 
 app.use(errorHandler);
 
